78d92d5b903e1a3ba205f5ff685abf87394431eb
Gabriel Silva de Jesus <ga.silvadejesus@gmail.com>
2025-02-24 21:13:04 -0300

    lista de nomes

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeNomes = [];
+
+function adicionarAmigo() {
+    const nomeInput = document.getElementById("amigo");
+    const nome = nomeInput.value.trim();
+    if (nome !== "") {
+        listaDeNomes.push(nome);
+        atualizarLista();
+        nomeInput.value = "";
+    }
+}
+
+function atualizarLista() {
+    const listaElement = document.getElementById("listaAmigos");
+    listaElement.innerHTML = "";
+    listaDeNomes.forEach((nome) => {
+        const li = document.createElement("li");
+        li.textContent = nome;
+        listaElement.appendChild(li);
+    });
+}
